ab0d9a5d72bca8374bd8684984449a793a45b018
andrsweb <d3vv.proo@gmail.com>
2024-09-16 00:32:47 +0700

    feat: installed AOS

--- src/html/sections/Home/Hero.tsx
+++ src/html/sections/Home/Hero.tsx
@@ -11,44 +11,64 @@ import b4 from '../../../assets/img/b4.jpg'
 import b5 from '../../../assets/img/b5.jpg'
 import star from '../../../assets/svg/logos/stargaze.svg'
 import Socials from './Socials'
+import { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 const Hero = () => {
-	return (
-		<section className="hero">
-			<img className='hero-bg' src={star} alt="" />
-			<div className="container">
-				<div className="hero-wrapper">
-					<div className="hero-images">
-						<img src={h2} alt="" />
-						<img src={b3} alt="" />
-						<img src={h3} alt="" />
-						<img src={b5} alt="" />
-						<img src={b4} alt="" />
-						<img src={h1} alt="" />
-						<img src={b1} alt="" />
-						<img src={h4} alt="" />
-						<img src={b2} alt="" />
-						<img src={h5} alt="" />
-					</div>
-					<div className="hero-blocks">
-						<div className="hero-blocks-top">
-							<h1>Reach new horizons on <a href="https://www.stargaze.zone/" target='_blank'>Stargaze</a> with</h1>
-							<div className='hero-subtitle'>Cryptomatcha</div>
-							<p>
-								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus commodi iure quaerat sequi nesciunt ad dicta. Eius ab quibusdam perferendis laboriosam porro! Ad suscipit quis dolorem tenetur fugiat voluptatem ex?
-							</p>
-							<p>
-								Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque labore libero harum expedita, ipsam, excepturi quibusdam enim at assumenda, odio mollitia nostrum voluptate. Saepe dicta laboriosam non voluptatem consectetur fugiat?
-							</p>
-						</div>
-						<div className="hero-blocks-bottom">
-							<Socials />
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	)
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, 
+      once: true,
+    })
+  }, [])
+
+  return (
+    <section className="hero">
+      <img className="hero-bg" src={star} alt="" />
+      <div className="container">
+        <div className="hero-wrapper">
+          <div className="hero-images" data-aos="fade-up">
+            <img src={h2} alt="" />
+            <img src={b3} alt="" />
+            <img src={h3} alt="" />
+            <img src={b5} alt="" />
+            <img src={b4} alt="" />
+            <img src={h1} alt="" />
+            <img src={b1} alt="" />
+            <img src={h4} alt="" />
+            <img src={b2} alt="" />
+            <img src={h5} alt="" />
+          </div>
+          <div className="hero-blocks" data-aos="fade-in">
+            <div className="hero-blocks-top">
+              <h1>
+                Reach new horizons on{' '}
+                <a href="https://www.stargaze.zone/" target="_blank" rel="noopener noreferrer">
+                  Stargaze
+                </a>{' '}
+                with
+              </h1>
+              <div className="hero-subtitle">Cryptomatcha</div>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus commodi iure quaerat
+                sequi nesciunt ad dicta. Eius ab quibusdam perferendis laboriosam porro! Ad suscipit
+                quis dolorem tenetur fugiat voluptatem ex?
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque labore libero harum
+                expedita, ipsam, excepturi quibusdam enim at assumenda, odio mollitia nostrum
+                voluptate. Saepe dicta laboriosam non voluptatem consectetur fugiat?
+              </p>
+            </div>
+            <div className="hero-blocks-bottom">
+              <Socials />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
