9cbe494179c0aed1e9a6aa2a054370fc2d367bc4
andrsweb <d3vv.proo@gmail.com>
2024-09-19 16:17:50 +0700

    upd: Created global cards section

--- src/html/pages/Home/Home.tsx
+++ src/html/pages/Home/Home.tsx
@@ -3,11 +3,13 @@ import Hero from "../../sections/Home/Hero/Hero"
 import Collaborations from "../../sections/Home/Collaborations/Collaborations"
 import AboutUs from "../../sections/Home/AboutUs/AboutUs"
 import Coin from "../../sections/Home/Coin/Coin"
+import GlobalCards from "../../sections/Home/GlobalCards/GlobalCards"
 
 const Home = () => {
 	return (
 		<div className="sections">
 			<Hero />
+			<GlobalCards />
 			<AboutUs />
 			<Coin />
 			<Collaborations />
---
+++ src/html/sections/Home/GlobalCards/GlobalCards.tsx
@@ -0,0 +1,45 @@
+
+import './GlobalCards.scss'
+
+const GlobalCards = () => {
+	return (
+		<section className="global-cards">
+			<div className="container">
+				<div className="global-cards-wrapper">
+					<div className="global-card">
+						<div className="global-card-inner">
+							<div className="global-card-title">
+								50 +
+							</div>
+							<p>
+								Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam sequi ab delectus recusandae obcaecati esse aperiam est ex doloribus! Nesciunt rerum sit aut laborum cupiditate excepturi aperiam quaerat voluptates dolor.
+							</p>
+						</div>
+					</div>
+					<div className="global-card">
+						<div className="global-card-inner">
+							<div className="global-card-title">
+								40 %
+							</div>
+							<p>
+								Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam sequi ab delectus recusandae obcaecati esse aperiam est ex doloribus! Nesciunt rerum sit aut laborum cupiditate excepturi aperiam quaerat voluptates dolor.
+							</p>
+						</div>
+					</div>
+					<div className="global-card">
+						<div className="global-card-inner">
+							<div className="global-card-title">
+								150 +
+							</div>
+							<p>
+								Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam sequi ab delectus recusandae obcaecati esse aperiam est ex doloribus! Nesciunt rerum sit aut laborum cupiditate excepturi aperiam quaerat voluptates dolor.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default GlobalCards
