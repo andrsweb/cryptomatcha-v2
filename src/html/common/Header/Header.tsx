ba059151fb8224e84bda132ca49d0bc8f0d3179f
andrsweb <d3vv.proo@gmail.com>
2024-09-24 06:24:47 +0700

    upd: layout changes

--- src/html/common/Header/Header.tsx
+++ src/html/common/Header/Header.tsx
@@ -1,17 +1,24 @@
+// Styles
 import './Header.scss'
 import "@interchain-ui/react/styles"
-import { WINDOW_INNER_WIDTH_XL } from "../../../global/constants"
+// Libs
 import { useState, useEffect, useRef } from "react"
 import { ChainRegistryClient } from "@chain-registry/client"
+import { Link } from 'react-router-dom'
+// Functions
+import { toggleBodyScroll } from '../../../global/functions'
+// Components
 import WalletButton from "../../components/buttons/WalletButton/WalletButton"
 import HeaderLogo from './HeaderLogo'
 import NavLeft from '../Nav/HeaderNav/NavLeft'
 import NavRight from '../Nav/HeaderNav/NavRight'
 import BorderLink from "../../components/Links/BorderLink"
+// Constants
+import { WINDOW_INNER_WIDTH_XL } from "../../../global/constants"
+// Icons/images
 import { FaBars, FaTimes } from "react-icons/fa"
-import { Link } from 'react-router-dom'
 import logo from '../../../assets/svg/logos/header-logo.svg'
-import { toggleBodyScroll } from '../../../global/functions'
+
 
 const Header = () => {
 	const [menuOpen, setMenuOpen] = useState(false)
