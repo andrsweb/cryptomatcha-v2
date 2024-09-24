8dbe11cb34e7de782ef3dbe3ffc2af27b1ed2bbc
andrsweb <d3vv.proo@gmail.com>
2024-09-24 22:57:06 +0700

    feat: coin presets

--- src/html/sections/Home/Coin/Logo3d.tsx
+++ src/html/sections/Home/Coin/Logo3d.tsx
@@ -11,9 +11,9 @@ export interface RotatingMeshProps {
 const RotatingMesh = ({ scene }: RotatingMeshProps) => {
 	const meshRef = useRef<THREE.Group>(null)
 	const [scale, setScale] = useState([1.3, 1.3, 1.3])
-	const rotationSpeed = useRef(0.06)
+	const rotationSpeed = useRef(0.25)
 	const swayAmplitude = 0.1
-	const swaySpeed = 1
+	const swaySpeed = 2.2
 
 	const updateScale = () => {
 		const width = window.innerWidth
@@ -39,10 +39,22 @@ const RotatingMesh = ({ scene }: RotatingMeshProps) => {
 	useFrame(({ clock }, delta) => {
 		if (meshRef.current) {
 			const elapsedTime = clock.getElapsedTime()
-			meshRef.current.rotation.y += rotationSpeed.current * delta
+			meshRef.current.rotation.y += rotationSpeed.current * delta 
 			meshRef.current.position.y = Math.sin(elapsedTime * swaySpeed) * swayAmplitude
 		}
 	})
+
+	useEffect(() => {
+		scene.traverse((child) => {
+		  if (child instanceof THREE.Mesh) {
+			child.material = new THREE.MeshStandardMaterial({
+			  metalness: 1, 
+			  roughness: 0.2,
+			  color: child.material.color
+			})
+		  }
+		})
+	  }, [])
 
 	return (
 		<primitive
@@ -50,7 +62,7 @@ const RotatingMesh = ({ scene }: RotatingMeshProps) => {
 			object={scene}
 			scale={scale}
 			position={[0, 0, 0]}
-			rotation={[2, Math.PI, 0.5]}
+			rotation={[2.2, Math.PI, 0.5]}
 		/>
 	)
 }
@@ -68,21 +80,7 @@ const Logo3D = () => {
 
 	return (
 		<Canvas className='canvas'>
-			<ambientLight intensity={0.5} />
-			<directionalLight
-				ref={lightRef}
-				position={[10, 10, 10]}
-				intensity={0.7}
-				castShadow={false}
-			/>
-			<pointLight
-				position={[1.5, 2, 1]}
-				intensity={1}
-				distance={20}
-				decay={1}
-				color="#ffffff"
-			/>
-			<Environment preset="night" />
+			<Environment preset="forest" />
 			<RotatingMesh scene={scene as THREE.Group} />
 		</Canvas>
 	)
